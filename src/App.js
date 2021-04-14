1d3fca6716a0b4e57c8192b732d311103d6f755d
Chayut Prawattiyakul <c.prawattiyakul@gmail.com>
2021-04-14 14:15:26 +0700

    FInished Update ans Delete Task function (but Ui is still not perfectly finish)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Amplify, { API, graphqlOperation, Auth } from "aws-amplify";
-import { createTodo, createUser, deleteUser } from "./graphql/mutations";
+import {
+  createTodo,
+  createUser,
+  deleteUser,
+  updateUser,
+} from "./graphql/mutations";
+import * as mutations from "./graphql/mutations";
 import { listTodos, listUsers } from "./graphql/queries";
 import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import awsExports from "./aws-exports";
 import { event } from "jquery";
 Amplify.configure(awsExports);
@@ -24,6 +32,11 @@ const App = () => {
   const [current, setCurrent] = useState({ uid: "" });
   const [todos, setTodos] = useState([]);
   const [currentTab, setCurrentTab] = useState("Task");
+  const [show, setShow] = useState(false);
+  const [forPopup, setForPopup] = useState(initialState);
+  // const [totalTask, settotalTask] = useState(0);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     getUsername();
@@ -39,7 +52,7 @@ const App = () => {
       const todoData = await API.graphql(graphqlOperation(listUsers));
       const todos = todoData.data.listUsers.items;
       setTodos(todos);
-      console.log("FetchTODo", todos);
+      console.log("FetchTodo", todos);
     } catch (err) {
       console.log("error fetching todos");
     }
@@ -61,7 +74,7 @@ const App = () => {
       console.log("error creating todo:", err);
     }
   }
-  //Still error
+
   async function deleteTask(cid) {
     try {
       const detail = { id: cid };
@@ -74,14 +87,38 @@ const App = () => {
     }
   }
 
+  async function editTask() {
+    console.log("EditFunction", forPopup);
+    try {
+      const temp = {
+        id: forPopup.id,
+        uid: forPopup.uid,
+        email: forPopup.email,
+        task: forPopup.task,
+        description: forPopup.description,
+        due_date: forPopup.due_date,
+        type: forPopup.type,
+      };
+      await API.graphql({
+        query: mutations.updateUser,
+        variables: { input: temp },
+      });
+      await fetchTodos();
+      alert("Success!");
+    } catch (err) {
+      console.log("error editing todo:", err);
+    }
+  }
+
   async function addClip() {
     try {
       if (!formState.task || !formState.description || !formState.due_date)
         return;
       const todo = { ...formState, type: "Clip" };
-      setTodos([...todos, todo]);
+      // setTodos([...todos, todo]);
       setFormState({ ...formState, task: "", description: "", due_date: "" });
       await API.graphql(graphqlOperation(createUser, { input: todo }));
+      await fetchTodos();
       alert("Success!");
       console.log("ADDtodo", todos);
     } catch (err) {
@@ -94,9 +131,10 @@ const App = () => {
       if (!formState.task || !formState.description || !formState.due_date)
         return;
       const todo = { ...formState, type: "Exam" };
-      setTodos([...todos, todo]);
+      // setTodos([...todos, todo]);
       setFormState({ ...formState, task: "", description: "", due_date: "" });
       await API.graphql(graphqlOperation(createUser, { input: todo }));
+      await fetchTodos();
       alert("Success!");
       console.log("ADDtodo", todos);
     } catch (err) {
@@ -122,12 +160,10 @@ const App = () => {
   }
 
   const display = () => {
-    // setFormState({initialState})
     switch (currentTab) {
       case "Task":
         return (
           <div style={styles.mgt}>
-            {" "}
             <input
               onChange={(event) => setInput("task", event.target.value)}
               style={styles.input}
@@ -154,36 +190,50 @@ const App = () => {
             </button>
             {todos.map((todo, index) =>
               todo.uid == current.uid && todo.type == "Task" ? (
-                <div key={todo.id ? todo.id : index} style={styles.todo}>
-                  <input
-                    class="noborder"
-                    disabled={true}
-                    style={styles.todoName}
-                    value={todos[index].task}
-                    type="text"
-                    onChange={(e) => {
-                      let temp = [...todos];
-                      temp[index].task = e.target.value;
-                      setTodos(temp);
-                      console.log(todos);
-                    }}
-                  />
-                  <p style={styles.todoDescription}>{todo.due_date}</p>
-                  <p style={styles.todoDescription}>{todo.description}</p>
-                  {console.log(todo.id)}
+                <div
+                  key={todo.id ? todo.id : index}
+                  style={styles.todo}
+                  className="d-flex p-4 justify-content-between"
+                >
+                  <p style={styles.todoName} className = "">{todo.task}</p>
+                  <div>
+                    <div className="d-flex flex-row">
+                      <p style={styles.todoDescription} className="font-bold">
+                        Due Date :
+                      </p>
+                      <p style={styles.todoDescription} className = "pl-1">{todo.due_date}</p>
+                    </div>
+                    <div className="d-flex flex-row">
+                      <p style={styles.todoDescription} className="font-bold">
+                        Description :
+                      </p>
+                      <p style={styles.todoDescription} className = "pl-1">{todo.description}</p>
+                    </div>
+                  </div>
 
-                  <button class="btn btn-success margin-2" onClick={() => {}}>
+                  {console.log(todo.id)}
+                  <div className="d-flex flex-row ">
+                  <div className = "pr-1">
+                  <button
+                    class="btn btn-success margin-2 fixed-size"
+                    onClick={() => {
+                      setForPopup(todo);
+                      handleShow();
+                      console.log(forPopup);
+                    }}
+                  >
                     Edit
                   </button>
-
+                  </div>
                   <button
-                    class="btn btn-danger"
+                    class="btn btn-danger fixed-size"
                     onClick={() => {
                       deleteTask(todo.id);
                     }}
                   >
                     Delete
                   </button>
+                </div>
                 </div>
               ) : (
                 <></>
@@ -200,7 +250,7 @@ const App = () => {
               onChange={(event) => setInput("task", event.target.value)}
               style={styles.input}
               value={formState.task}
-              placeholder="Task"
+              placeholder="Clip"
             />
             <input
               type="text"
@@ -226,6 +276,25 @@ const App = () => {
                   <p style={styles.todoName}>{todo.task}</p>
                   <p style={styles.todoDescription}>{todo.due_date}</p>
                   <p style={styles.todoDescription}>{todo.description}</p>
+                  <button
+                    class="btn btn-success margin-2"
+                    onClick={() => {
+                      setForPopup(todo);
+                      handleShow();
+                      console.log(forPopup);
+                    }}
+                  >
+                    Edit
+                  </button>
+
+                  <button
+                    class="btn btn-danger"
+                    onClick={() => {
+                      deleteTask(todo.id);
+                    }}
+                  >
+                    Delete
+                  </button>
                 </div>
               ) : (
                 <></>
@@ -242,7 +311,7 @@ const App = () => {
               onChange={(event) => setInput("task", event.target.value)}
               style={styles.input}
               value={formState.task}
-              placeholder="Task"
+              placeholder="Exam"
             />
             <input
               type="text"
@@ -268,6 +337,25 @@ const App = () => {
                   <p style={styles.todoName}>{todo.task}</p>
                   <p style={styles.todoDescription}>{todo.due_date}</p>
                   <p style={styles.todoDescription}>{todo.description}</p>
+                  <button
+                    class="btn btn-success margin-2"
+                    onClick={() => {
+                      setForPopup(todo);
+                      handleShow();
+                      console.log(forPopup);
+                    }}
+                  >
+                    Edit
+                  </button>
+
+                  <button
+                    class="btn btn-danger"
+                    onClick={() => {
+                      deleteTask(todo.id);
+                    }}
+                  >
+                    Delete
+                  </button>
                 </div>
               ) : (
                 <></>
@@ -278,9 +366,59 @@ const App = () => {
     }
   };
 
+  const popup = () => {
+    return (
+      <Modal show={show} onHide={handleClose} size="lg" centered>
+        <Modal.Header closeButton>
+          <Modal.Title> Edit detail</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input
+            onChange={(event) =>
+              setForPopup({ ...forPopup, task: event.target.value })
+            }
+            value={forPopup.task}
+            style={styles.input}
+          />
+          <input
+            type="text"
+            onFocus={(event) => (event.currentTarget.type = "date")}
+            onBlur={(event) => (event.currentTarget.type = "text")}
+            onChange={(event) =>
+              setForPopup({ ...forPopup, due_date: event.target.value })
+            }
+            value={forPopup.due_date}
+            style={styles.input}
+          />
+          <input
+            onChange={(event) =>
+              setForPopup({ ...forPopup, description: event.target.value })
+            }
+            value={forPopup.description}
+            style={styles.input}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              editTask();
+              handleClose();
+            }}
+          >
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+
   return (
     <div style={styles.container}>
-      <h2>Welcome!</h2>
+      <h2>Welcome {current.uid} !</h2>
       <ul className="nav nav-pills nav-fill">
         <li className="nav-item">
           <a
@@ -345,6 +483,7 @@ const App = () => {
       </ul>
       {/* <button onClick={getUsername}>pim</button> */}
       <div className="pane-area-container "> {display()} </div>
+      <div> {popup()} </div>
       <AmplifySignOut />
     </div>
   );
